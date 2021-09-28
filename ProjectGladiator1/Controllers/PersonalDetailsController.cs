4e4b26d52ef88fa162f460c31b7af53c5cc695e6
Prudhvi16G <saiprudhvi.gundeti99@gmail.com>
2021-09-28 12:05:44 +0530

    Add project files.

---
+++ ProjectGladiator1/Controllers/PersonalDetailsController.cs
@@ -0,0 +1,34 @@
+﻿using ProjectGladiator1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectGladiator1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class PersonalDetailsController:ControllerBase
+    {
+        Home_LoanContext context;
+        public PersonalDetailsController(Home_LoanContext _context)
+        {
+            context = _context;
+        }
+        public IActionResult GetAll()
+        {
+            return Ok(context.PersonalDetails);
+        }
+        [HttpPost("PersonalDetails")]
+        public IActionResult perdetails(PersonalDetails personaldetails)
+        {
+            context.PersonalDetails.Add(personaldetails);
+            context.SaveChanges();
+            return Ok(context.PersonalDetails);
+        }
+    }
+}
---
+++ ProjectGladiator1/Models/LoanDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ProjectGladiator1.Models
+{
+    public partial class LoanDetails
+    {
+        public LoanDetails()
+        {
+            ApplicationDetails = new HashSet<ApplicationDetails>();
+        }
+
+        public int ApplicationId { get; set; }
+        public string Username { get; set; }
+        public int? MaxLoanAmountGrantable { get; set; }
+        public decimal? InterestRate { get; set; }
+        public int? Tenure { get; set; }
+        public int? LoantAmount { get; set; }
+        public DateTime? LoanStartDate { get; set; }
+
+        public virtual PersonalDetails UsernameNavigation { get; set; }
+        public virtual ICollection<ApplicationDetails> ApplicationDetails { get; set; }
+    }
+}
